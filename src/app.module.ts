3eedd2fbcfacf61786b596c40cc348664cd0c421
Henrique Pagani <henriqueptho44@hotmail.com>
2025-06-12 13:45:03 -0300

    App Module and Package.Json updates - ConfigModule and JWT Token

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,28 +9,39 @@ import { PedidoModule } from './modules/pedido/pedido.module';
 import { ItensCarrinhoModule } from './modules/itens_carrinho/itens_carrinho.module';
 import { UsersModule } from './modules/users/users.module';
 import { EnderecoModule } from './modules/endereco/endereco.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'; //para o .env
+import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '515253',
-      database: 'well_dressed',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false, // ⚠️ NÃO sobrescreve seu banco
-      autoLoadEntities: true, // Carrega automaticamente as entidades dos módulos
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Importe ConfigModule aqui para acessá-lo
+      useFactory: (configService: ConfigService) => ({ //atributo do forRootAsync
+        type: configService.get<any>('DB_CONNECTION'),
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT', '3306'), 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], 
+      synchronize: false, //NÃO sobrescreve o banco
+      autoLoadEntities: true, // carrega automaticamente as entidades dos módulos
+      }),
+      inject: [ConfigService], // Injete o ConfigService, atributo do forRootAsync
+  }),
+    AuthModule,
     ProdutoModule,
     ItensProdutoModule,
     ItensCarrinhoModule,
     CarrinhoModule,
     PedidoModule,
     UsersModule,
-    EnderecoModule
+    EnderecoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
